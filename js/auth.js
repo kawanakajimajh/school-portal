e661cac67d4153a176b7d6408fa0953f9de7dabb
kawanakajimajh <tanzinza@outlook.jp>
2026-07-23 18:47:56 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,39 @@
+// =======================================
+// 川中島中学校 CMS Version4
+// auth.js
+// =======================================
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email =
+        document.querySelector("#email").value;
+
+    const password =
+        document.querySelector("#password").value;
+
+    const { data, error } =
+        await db.auth.signInWithPassword({
+
+            email: email,
+
+            password: password
+
+        });
+
+    if (error) {
+
+        alert("メールアドレスまたはパスワードが違います。");
+
+        return;
+
+    }
+
+    alert("ログイン成功！");
+
+    location.href = "dashboard.html";
+
+});
